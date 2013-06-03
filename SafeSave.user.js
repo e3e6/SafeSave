10c0d96c2b4863086bc1686451480c7d46a97ac2
Alexander <xandrs.box@gmail.com>
2013-06-04 02:07:18 +0300

    Update SafeSave.user.js

--- SafeSave.user.js
+++ SafeSave.user.js
@@ -13,8 +13,74 @@
 //document.getElementsByTagName("head")[0].appendChild(jq);
 
 
-var script = document.createElement("script");
-    //script.src = "http://localhost:801/external/safeSave.js"; //my internal link for development
-    script.src = "https://ae-yoda.atlassian.net/wiki/download/attachments/3604521/safeSave.js";
+//var script = document.createElement("script");
+   // script.src = "http://localhost:801/external/safeSave.js"; //my internal link for development
+//    script.src = "https://ae-yoda.atlassian.net/wiki/download/attachments/3604521/safeSave.js";
 
-document.getElementsByTagName("head")[0].appendChild(script);
+//document.getElementsByTagName("head")[0].appendChild(script);
+
+/**
+ * Add codemirror
+ */
+
+var scriptsLocal = ["http://localhost:801/external/safeSave.js"];
+
+
+var scripts = ["https://raw.github.com/e3e6/SafeSave/master/safeSave.js",
+  //             "http://localhost:801/external/codemirror.js",
+ //              "http://localhost:801/external/codemirror-ui.js",
+
+ //              "http://localhost:801/external/javascript.js"
+//                              ,"http://localhost:801/external/init-mirror.js"
+              ];
+var csss = ["http://localhost:801/external/codemirror.css",
+            "http://localhost:801/external/codemirror-ui.css"
+           ]; 
+/**
+ * Inser script element to page
+ */ 
+function insertScriptSrc(scriptsList){
+  var length = scriptsList.length;
+  var element = null;
+  for (var i = 0; i < length; i++) {
+    element = scriptsList[i];
+    debug(">> append: " + element);
+    var script = document.createElement("script");
+        script.src = element;
+
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+}
+/**
+ * 
+ */
+function insertCssSrc(scriptsList){
+  var length = scriptsList.length;
+  var element = null;
+  for (var i = 0; i < length; i++) {
+    element = scriptsList[i];
+    debug(">> append: " + element);
+    var fileref=document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", element);
+          
+    document.getElementsByTagName("head")[0].appendChild(fileref);
+  }
+}
+
+/**
+ * Write to console if exist
+ */
+function debug(text){
+  if(window.console){
+    console.info(text);
+  }
+}
+
+/**
+ * Entry
+ */
+insertScriptSrc(scripts);
+//insertScriptSrc(scriptsLocal);
+//insertCssSrc(csss); 
